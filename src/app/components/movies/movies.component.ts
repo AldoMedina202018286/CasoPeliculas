5f4c3bb6b3a0c795450c4f717bc2a6cd2fbebac0
Aldo Medina <u202018286@upc.edu.pe>
2023-04-28 19:31:24 -0500

    Trabajo en parejas 28/04/2023

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -2,10 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { NgForm } from '@angular/forms';
 
 import { Movie } from '../../models/movies.model';
-import {HttpDataService} from "../../services/http-data.service";
 
 
 @Component({
@@ -16,15 +14,17 @@ import {HttpDataService} from "../../services/http-data.service";
 export class MoviesComponent implements OnInit {
 
   movies: Movie[] = [
-    { id:'1', title: 'Pelicula 1', duration: 115, genre: 'suspense' },
-    { id:'2', title: 'Pelicula 2', duration: 176, genre: 'action' },
-    { id:'3', title: 'Pelicula 3', duration: 125, genre: 'romance' },
+    { id:'1', poster: 'https://www.themoviedb.org/t/p/w220_and_h330_face/jXbqfVHmvCikyTw2Lf5OhKyt9Ym.jpg', title: 'The Social Network', duration: 121, genre: 'Biography, Drama' },
+    { id:'2', poster: 'https://www.themoviedb.org/t/p/w220_and_h330_face/34m2tygAYBGqA9MXKhRDtzYd4MR.jpg', title: 'The Wolf of Wall Street ', duration: 180, genre: 'Biography, Comedy, Crime, Drama' },
+    { id:'3', poster: 'https://www.themoviedb.org/t/p/w220_and_h330_face/ctjEj2xM32OvBXCq8zAdK3ZrsAj.jpg', title: 'Catch Me If You Can', duration: 141, genre: 'Biography, Crime, Drama' },
+    { id:'4', poster: 'https://www.themoviedb.org/t/p/w220_and_h330_face/tWsNYbrqy1p1w6K9zRk0mSchztT.jpg', title: 'Now You See Me', duration: 115, genre: 'Crime, Mystery, Thriller' },
+    { id:'5', poster: 'https://www.themoviedb.org/t/p/w220_and_h330_face/zqDopwg7XQ4IfFX2dRlQCT1SwMG.jpg', title: 'The Girl with the Dragon Tattoo', duration: 158, genre: 'Thriller, Crime, Mystery' },
   ];
 
   movieData!: Movie;
   // dataSource = new MatTableDataSource();
   dataSource: MatTableDataSource<Movie>;
-  displayedColumns: string[] = ['id', 'title', 'duration', 'genre', 'actions'];
+  displayedColumns: string[] = ['id', 'poster', 'title', 'duration', 'genre', 'actions'];
 
   @ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort!: MatSort;
@@ -35,21 +35,10 @@ export class MoviesComponent implements OnInit {
     this.dataSource = new MatTableDataSource<Movie>(this.movies);
   }
 
-  /*constructor(private httpDataService: HttpDataService) {
-    this.movieData = { } as Movie;
-  }*/
-
   ngOnInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
-    // this.getAllMovies();
   }
-
-  /*getAllMovies() {
-    this.httpDataService.getList().subscribe((response: any) => {
-      this.dataSource.data = response;
-    })
-  }*/
 
   editMovie(id: string, movie: Movie) {
     const index = this.movies.findIndex(m => m.id === id);
--- src/app/models/movies.model.ts
+++ src/app/models/movies.model.ts
@@ -1,5 +1,6 @@
 export interface Movie {
   id: any;
+  poster: any;
   title: any;
   duration: any;
   genre: any;
